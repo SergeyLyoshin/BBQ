236172308c9ff00a351b669d5396783bf71638be
slyoshin <slyoshin@icloud.com>
2022-11-02 16:34:01 +0500

    controller/views/routes photos added, avatar

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,6 +6,7 @@ class Event < ApplicationRecord
 	# Чтобы Рельсы понимали, какой именно класс будет лежать
 	# в модели subscribers, надо указать source
 	has_many :subscribers, through: :subscriptions, source: :user
+	has_many :photos 
 		
 	validates :title, presence: true, length: {maximum: 255}
 	validates :address, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :photos
   resources :subscriptions
   resources :comments
   devise_for :users
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
     # вложенный ресурс подписок
     resources :subscriptions, only: [:create, :destroy]
+    # Вложенные в ресурс события ресурсы фотографий
+    resources :photos, only: [:create, :destroy]
   end
   
   resources :users, only: %i[show edit update]
